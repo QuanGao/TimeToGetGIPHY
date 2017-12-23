d9b55fb4143f28fbea906326162a8dbbdd3ff7ad
Quan Gao <quangao@Quans-MacBook-Pro.local>
2017-12-22 22:45:52 -0600

    add all preload buttons in html via jquery

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,9 +1,23 @@
+$(document).ready(function(){
 var movies = ["Lion King","Frozen","Mulan","Aladin","The Little Mermaid","Beauty and the Beast","Moana",
-    "Bambi","Zootopia","Sleeping Beauty","Finding Nemo"]
+    "Bambi","Zootopia","Sleeping Beauty","Finding Nemo", "Cinderella","Pinocchio","Lady and the Tramp"]
 
-$.ajax({
-    url:"",
-    method: "GET"
-}).done(function(){
+var addbutton = function(buttonName){
+    var newbutton = $("<button>");
+    newbutton.text(buttonName);
+    $(".buttons").append(newbutton);
+}
 
-})
+for (var i=0; i < movies.length; i++){
+    addbutton(movies[i]);
+};
+
+var queryURL = 
+
+// $.ajax({
+//     url:"",
+//     method: "GET"
+// }).done(function(){
+
+// })
+});
