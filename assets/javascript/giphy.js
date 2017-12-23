b3f0e7c0c4586d20bbf70df845a904454d324c7f
Quan Gao <quangao@Quans-MacBook-Pro.local>
2017-12-22 19:46:55 -0600

    add html structure, css for the header and side bar

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,9 @@
+var movies = ["Lion King","Frozen","Mulan","Aladin","The Little Mermaid","Beauty and the Beast","Moana",
+    "Bambi","Zootopia","Sleeping Beauty","Finding Nemo"]
+
+$.ajax({
+    url:"",
+    method: "GET"
+}).done(function(){
+
+})
